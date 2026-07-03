d79f572c9e38ba07d7c9051f391d021debc1cb6e
Venkata Manikanta Batlanki <onlygodsb2k.741@gmail.com>
2026-07-03 17:44:00 +0530

    Delete Ruff directory

--- Ruff/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RuffConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Ruff'
--- Ruff/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def Rufffun(request):
-    return render(request,'Ruff/Ruff.html')
