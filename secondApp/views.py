e33878b3dc33765152c3b22af97dd7c4fbb2db50
Venkata Manikanta Batlanki <onlygodsb2k.741@gmail.com>
2026-07-03 17:46:04 +0530

    Delete secondApp directory

--- secondApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def secondfun(request):
-    obj=render(request,'second.html')
-    return obj
